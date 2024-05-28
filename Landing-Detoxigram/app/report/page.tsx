8badeb000e4c8363298351750eedf567ac807d9a
Luz Alba Posse <luzalbaposse@gmail.com>
2024-05-28 13:48:34 -0300

    bg report

--- Landing-Detoxigram/app/report/page.tsx
+++ Landing-Detoxigram/app/report/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
                 {/* Div Número 6 */}
                 <div className="col-span-6">
                     <div className="flex flex-wrap gap-4 content-start bg-neutral-300/20 relative p-8 sm:p-12 border aspect-video md:aspect-[3/1] border-neutral-400/30 rounded-xl shadow-sm overflow-hidden">
-                        <div className="image-container absolute flex w-full h-full inset-y-0 bottom-0 items-center  right-0 flex-1">
+                        <div className="bg-[url('/bg-report.png')] bg-cover bg-center bg-no-repeat image-container absolute flex w-full h-full inset-y-0 bottom-0 items-center  right-0 flex-1">
                         <span className="ml-10 w-3/5 block text-xl sm:text-4xl">Learn about our project</span>
                         </div>
                     </div>
