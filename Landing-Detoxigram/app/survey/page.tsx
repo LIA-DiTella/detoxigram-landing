d61e691ea53bb95bdf8b2c472adb06429f6a467b
PattoPeraltta <136608427+PattoPeraltta@users.noreply.github.com>
2024-05-30 12:26:28 -0300

    Agregue los segundos graficos

--- Landing-Detoxigram/app/survey/page.tsx
+++ Landing-Detoxigram/app/survey/page.tsx
@@ -15,9 +15,6 @@ export default function Home() {
 
                 {/* Intro Text */}
                 <div className="col-span-6">
-                        <div className=" border border-neutral-400/30 rounded-xl shadow-sm overflow-hidden">
-                            <h1 className="w-full text-center">HOLA</h1>
-                        </div>
                         <p className= "w-full text-xl text-left mt-5 mb-5">
                             Firstly, it's important to know who we were surveying. In other words, the sampling. The survey was answered by 307 participants. The following graphs intend to give a
                             better image of the distribution of them in different categories:
@@ -77,33 +74,64 @@ export default function Home() {
 
                 {/* Analysis Text */}
                 <div className="col-span-6">
-                    <div className="bg-neutral-300/20 p-8 sm:p-12 border border-neutral-400/30 rounded-xl shadow-sm">
-                        <p>
-                            Firstly, we wondered if there existed a relationship between the political affiliation of participants, and their responses. To do this, we divided their judging of the
+                        <p className= "w-full text-xl text-left mt-5 mb-5">
+                            First, we wondered if there existed a relationship between the political affiliation of participants, and their responses. To do this, we divided their judging of the
                             sentences in two large categories, these being "Toxic" or "Non-toxic". We only focused on Democrats and Republicans, to see how the responses varied amongst them.
                         </p>
-                        <p className="mt-4 font-bold">
-                            In 8 of the 30 sentences that the participants judged, we could establish a significant difference between their answers and their affiliation. Here are some of these
-                            sentences and the distribution of responses:
+                        <p className= "w-full text-xl text-left mt-5 mb-5">
+                            In 8 of the 30 sentences that the participants judged, we could establish a significant difference between their answers and their affiliation. Here are some of the
+                            sentences and their distribution that had really pronounced difference:
                         </p>
-                    </div>
                 </div>
 
                 {/* Detailed Analysis Graphs */}
                 <div className="col-span-6 grid grid-cols-1 gap-4">
                     <div className="bg-neutral-300/20 p-8 sm:p-12 border border-neutral-400/30 rounded-xl shadow-sm">
-                        <p className="mb-4">Q0: Text of the question</p>
-                        <Image src="/path/to/your/detailed-analysis-graph-1.png" alt="Detailed Analysis Graph 1" layout="responsive" width={500} height={500} className="mx-auto" />
+                        <p className="mb-4">Participants judged the sentence:</p>
+                        <p className="mb-4 text-center bg-gray-300 px-2 py-1 rounded-md">
+                        "Exactly the reason my 5.56 and 7.62 and 9mm will never be given up. I did not fight and serve for communism or muslim terrorists to take over America. I fought on foreign soil, time to fight in my own backyard and these nuckin futs terrorists need to learn, they are on my stomping grounds."
+                         </p>
+                        <iframe 
+                            src='https://flo.uri.sh/visualisation/18176640/embed' 
+                            title='CAMBIAR' 
+                            className='flourish-embed-iframe' 
+                            frameBorder='0' 
+                            scrolling='no' 
+                            style={{ width: '100%', height: '600px' }} 
+                            sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'
+                        ></iframe>
                     </div>
 
                     <div className="bg-neutral-300/20 p-8 sm:p-12 border border-neutral-400/30 rounded-xl shadow-sm">
-                        <p className="mb-4">Q1: Text of the question</p>
-                        <Image src="/path/to/your/detailed-analysis-graph-2.png" alt="Detailed Analysis Graph 2" layout="responsive" width={500} height={500} className="mx-auto" />
+                    <p className="mb-4">Participants judged the sentence:</p>
+                        <p className=" text-center mb-4 bg-gray-300 px-2 py-1 rounded-md">
+                        "Trans is a mental disorder"
+                         </p>
+                        <iframe 
+                            src='https://flo.uri.sh/visualisation/18177064/embed'
+                            title='CAMBIAR' 
+                            className='flourish-embed-iframe' 
+                            frameBorder='0' 
+                            scrolling='no' 
+                            style={{ width: '100%', height: '600px' }} 
+                            sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'
+                        ></iframe>
                     </div>
 
                     <div className="bg-neutral-300/20 p-8 sm:p-12 border border-neutral-400/30 rounded-xl shadow-sm">
-                        <p className="mb-4">Q2: Text of the question</p>
-                        <Image src="/path/to/your/detailed-analysis-graph-3.png" alt="Detailed Analysis Graph 3" layout="responsive" width={500} height={500} className="mx-auto" />
+                    <p className="mb-4">Participants judged the sentence:</p>
+                    <p className=" text-center mb-4 bg-gray-300 px-2 py-1 rounded-md">
+                        "Exactly the reason my 5.56 and 7.62 and 9mm will never be given up. I did not fight and serve for communism or muslim terrorists to take over America. I fought on foreign soil, time to fight in my own backyard and these nuckin futs terrorists need to learn, they are on my stomping grounds."
+                    </p>
+                        <iframe 
+                            src='https://flo.uri.sh/visualisation/18176640/embed' 
+                            title='CAMBIAR' 
+                            className='flourish-embed-iframe' 
+                            frameBorder='0' 
+                            scrolling='no' 
+                            style={{ width: '100%', height: '600px' }} 
+                            sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'
+                        ></iframe>
                     </div>
                 </div>
             </div>
